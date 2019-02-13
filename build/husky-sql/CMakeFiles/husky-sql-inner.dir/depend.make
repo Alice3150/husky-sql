47b662c54c9e78d47e9adf08ba29c860c9100050
Alice3150 <alice.my.guan@gamil.com>
2019-02-13 17:40:31 +0800

    add husky-sql basic files

--- build/husky-sql/CMakeFiles/husky-sql-inner.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for husky-sql-inner.
-# This may be replaced when dependencies are built.
--- build/husky/third_party/src/glog-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /data/opt/tmp/1155107959/husky-sql/build/husky/third_party/src/glog
-# Build directory: /data/opt/tmp/1155107959/husky-sql/build/husky/third_party/src/glog-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
